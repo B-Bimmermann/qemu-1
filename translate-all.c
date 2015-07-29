6d988359a4d420f54ed0c099afe9465dcc8fbada
Pranith Kumar <bobby.prani@gmail.com>
2015-07-29 13:37:56 -0400

    Fix bug caused by tb_flush()

--- translate-all.c
+++ translate-all.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
- */
+ */ 
 #ifdef _WIN32
 #include <windows.h>
 #endif
