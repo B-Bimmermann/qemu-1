55baa605cca51835233f75501717f6e3b8185cc6
Alvise Rigo <a.rigo@virtualopensystems.com>
2015-08-14 16:46:58 +0200

    softmmu_llsc_template.h: move to multithreading
    Update the TCG LL/SC helpers to work in multi-threading.
    The basic idea remains untouched, but the whole mechanism takes now into
    account of the multiple, concurrent, vCPUs execution.
    
    In essence, if a vCPU does a LL it checks the vCPUs that have not the
    excl bit set for the accessed page. For those vCPUs it then:
    - sets the excl bit
    - queries a TLB flush
    
    Doing so, we make sure that all the vCPUs will have the EXCL flag in the
    TLB entry for that specific page *before* entering the next TB
    
    Changes from v3:
    - The rendez-vous mechanism has been removed since the reworked
      TLB flush query addresses the same purpose.
    
    Changes form v4:
    - Minor improvements
    
    Suggested-by: Jani Kokkonen <jani.kokkonen@huawei.com>
    Suggested-by: Claudio Fontana <claudio.fontana@huawei.com>
    Signed-off-by: Alvise Rigo <a.rigo@virtualopensystems.com>

--- softmmu_llsc_template.h
+++ softmmu_llsc_template.h
@@ -52,6 +52,20 @@
 
 #endif
 
+#define is_read_tlb_entry_set(env, page, index)                              \
+({                                                                           \
+    (addr & TARGET_PAGE_MASK)                                                \
+         == ((env->tlb_table[mmu_idx][index].addr_read) &                    \
+                 (TARGET_PAGE_MASK | TLB_INVALID_MASK));                     \
+})
+/* Whenever a SC operation fails, we add a small delay to reduce the
+ * concurrency among the atomic instruction emulation code. Without this delay,
+ * in very congested situation where plain stores make all the pending LLs
+ * fail, the code could reach a stalling situation in which all the SCs happen
+ * to fail.
+ * */
+#define TCG_ATOMIC_INSN_EMUL_DELAY 100
+
 WORD_TYPE helper_ldlink_name(CPUArchState *env, target_ulong addr,
                                 TCGMemOpIdx oi, uintptr_t retaddr)
 {
@@ -61,33 +75,44 @@ WORD_TYPE helper_ldlink_name(CPUArchState *env, target_ulong addr,
     hwaddr hw_addr;
     unsigned mmu_idx = get_mmuidx(oi);
 
-    /* Use the proper load helper from cpu_ldst.h */
-    ret = helper_ld(env, addr, mmu_idx, retaddr);
-
     index = (addr >> TARGET_PAGE_BITS) & (CPU_TLB_SIZE - 1);
+
+    if (!is_read_tlb_entry_set(env, addr, index) ||
+                        !VICTIM_TLB_HIT(addr_read)) {
+        tlb_fill(ENV_GET_CPU(env), addr, READ_ACCESS_TYPE, mmu_idx, retaddr);
+    }
 
     /* hw_addr = hwaddr of the page (i.e. section->mr->ram_addr + xlat)
      * plus the offset (i.e. addr & ~TARGET_PAGE_MASK) */
     hw_addr = (env->iotlb[mmu_idx][index].addr & TARGET_PAGE_MASK) + addr;
 
-    cpu_physical_memory_clear_excl_dirty(hw_addr, ENV_GET_CPU(env)->cpu_index);
+    qemu_mutex_lock(&tcg_excl_access_lock);
     /* If all the vCPUs have the EXCL bit set for this page there is no need
      * to request any flush. */
-    if (cpu_physical_memory_excl_is_dirty(hw_addr, smp_cpus)) {
+    if (unlikely(!atomic_xchg(&exit_flush_request, 1) &&
+        cpu_physical_memory_excl_is_dirty(hw_addr, smp_cpus))) {
         CPU_FOREACH(cpu) {
-            if (current_cpu != cpu) {
+            if (cpu->thread_id != qemu_get_thread_id()) {
                 if (cpu_physical_memory_excl_is_dirty(hw_addr,
-                                                    cpu->cpu_index)) {
-                    cpu_physical_memory_clear_excl_dirty(hw_addr,
-                                                         cpu->cpu_index);
-                    tlb_flush(cpu, 1);
+                                                      cpu->cpu_index)) {
+                    tlb_query_flush_cpu(cpu, 1);
                 }
             }
         }
+
+        cpu_physical_memory_clear_excl_dirty(hw_addr, smp_cpus);
+        atomic_set(&exit_flush_request, 0);
     }
+
+    env->ll_sc_context = true;
+
+    /* Use the proper load helper from cpu_ldst.h */
+    ret = helper_ld(env, addr, mmu_idx, retaddr);
 
     env->excl_protected_range.begin = hw_addr;
     env->excl_protected_range.end = hw_addr + DATA_SIZE;
+
+    qemu_mutex_unlock(&tcg_excl_access_lock);
 
     /* For this vCPU, just update the TLB entry, no need to flush. */
     env->tlb_table[mmu_idx][index].addr_write |= TLB_EXCL;
@@ -112,6 +137,7 @@ WORD_TYPE helper_stcond_name(CPUArchState *env, target_ulong addr,
         env->excl_succeeded = 0;
         ret = 0;
     } else {
+        g_usleep(TCG_ATOMIC_INSN_EMUL_DELAY);
         ret = 1;
     }
 
