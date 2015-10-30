stsquad/qemu

translate-all: (wip) use tb_flush_safe when we can't alloc more tb.
This changes just the tb_flush called from tb_alloc.

TODO:
 * changes the other tb_flush.

Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

translate-all.c
@@ -1147,7 +1147,7 @@ TranslationBlock *tb_gen_code(CPUState *cpu,
     tb = tb_alloc(pc);
     if (!tb) {
         /* flush must be done */
-        tb_flush(env);
+        tb_flush_safe(env);
         /* cannot fail at this point */
         tb = tb_alloc(pc);
         /* Don't forget to invalidate previous TB info.  */

B-Bimmermann/qemu-1

translate-all.c
@@ -1181,7 +1181,7 @@ TranslationBlock *tb_gen_code(CPUState *cpu,
     if (unlikely(!tb)) {
  buffer_overflow:
         /* flush must be done */
-        tb_flush(cpu);
+        tb_flush_safe(cpu);
         /* cannot fail at this point */
         tb = tb_alloc(pc);
         assert(tb != NULL);