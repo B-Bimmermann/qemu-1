stsquad/qemu

translate-all: introduces tb_flush_safe.
tb_flush is not thread safe we definitely need to exit VCPUs to do that.
This introduces tb_flush_safe which just creates an async safe work which will
do a tb_flush later.

Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

translate-all.c
@@ -815,6 +815,21 @@ static void page_flush_tb(void)
     }
 }
 
+static void tb_flush_work(void *opaque)
+{
+    CPUArchState *env = opaque;
+    tb_flush(env);
+}
+
+void tb_flush_safe(CPUArchState *env)
+{
+#if 0 /* !MTTCG */
+    tb_flush(env);
+#else
+    async_run_safe_work_on_cpu(ENV_GET_CPU(env), tb_flush_work, env);
+#endif /* MTTCG */
+}
+
 /* flush all the translation blocks */
 /* XXX: tb_flush is currently not thread safe */
 void tb_flush(CPUArchState *env1)

B-Bimmermann/qemu-1

translate-all.c
@@ -846,6 +846,21 @@ static void page_flush_tb(void)
     }
 }
 
+static void tb_flush_work(void *opaque)
+{
+    CPUState *cpu = opaque;
+    tb_flush(cpu);
+}
+
+void tb_flush_safe(CPUState *cpu)
+{
+#if 0 /* !MTTCG */
+    tb_flush(cpu);
+#else
+    async_run_safe_work_on_cpu(cpu, tb_flush_work, cpu);
+#endif /* MTTCG */
+}
+
 /* flush all the translation blocks */
 /* XXX: tb_flush is currently not thread safe */
 void tb_flush(CPUState *cpu)