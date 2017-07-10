489297b4b81c5a30a5045c3d4feed9a06bde1091
Benjamin Bimmermann <b.bimmermann@yahoo.de>
2017-07-10 12:08:28 +0200

    Remove unused blank that comes from my rebase.

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -8,7 +8,7 @@
  *
  * This work is licensed under the terms of the GNU GPL, version 2 or later.
  * See the COPYING file in the top-level directory.
- */ 
+ */
 
 #include "qemu/osdep.h"
 #include "hw/boards.h"
